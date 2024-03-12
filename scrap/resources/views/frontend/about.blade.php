d2f9db6fa652031ee0616284d7e1ee88261a25a6
sanketsaveasweb <sanket.saveasweb24@gmail.com>
2024-03-12 15:51:35 +0530

    working on scrap[

--- scrap/resources/views/frontend/about.blade.php
+++ scrap/resources/views/frontend/about.blade.php
@@ -1,38 +1,51 @@
 <x-frontend-header />
-<div class="container">
-    <div class="row">
-        <div class="col-md-12 mt-5">
-            <h3 class="text-decoration-underline slide-in-left">About Us</h3>
+
+<section style="background-color: #f8f9fb; height:100vh; ">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 mt-5">
+                <h5 class="slide-in-left h2"><span style="color:#4baf47">About Us</span></h5>
+            </div>
         </div>
     </div>
-</div>
-<div class="container mb-5">
-    <div class="row mt-4">
-        <div class="col-md-6 slide-in-left">
-            <img class="img-thumbnail zoom-in-out" src="{{ asset('frontend/theam/assets/images/about-sell/about.jpg') }}" width="520px"
-                alt="">
+    <div class="container mb-2">
+        <div class="row mt-4">
+
+            <div class="col-md-6 slide-in-left">
+                <p style="color:black; font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;">
+                <h2>Welcome To Scrap 24x7</h2>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro, maxime quasi. Sequi non quam vero est
+                saepe
+                soluta adipisci ea expedita, dolores possimus accusamus placeat laboriosam et provident eligendi veniam
+                in
+                voluptates eius quidem. Consequatur libero ratione, autem ducimus, recusandae optio voluptatem corrupti
+                aliquid molestiae pariatur sequi.<br>
+                Eligendi sunt odio ea repellat pariatur dolorum consectetur. Et dolorem,suscipit similique amet
+                accusantium minus ipsum! Ipsam tempore illo totam sit non eos maiores et modi
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas, quidem.
+
+                </p>
+                <div class="">
+                    <a href="#" class="btn btn-outline-success mt-4 justify-content-center">Sell Scrap</a>
+                </div>
+            </div>
+            <div class="col-md-6 slide-in-left">
+                <img class="zoom-in-out image-scrap rounded"
+                    src="{{ asset('frontend/theam/assets/images/about-sell/scrap-about11.jpg') }}" width="520px" height="1000px"
+                    alt="">
+            </div>
+            {{-- <div class="col-md-12 slide-in-right mt-3">
+                <p class="mt-1" style="color:black;">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, perspiciatis. Cumque illum impedit qui
+                    optio, et tempora asperiores eaque, accusantium veritatis ex quaerat sequi repellendus vero ipsum saepe
+                    voluptates. Nisi nostrum ullam totam quibusdam molestiae alias iure sit odio! Quia ad veniam dolor
+                </p>
+            </div> --}}
+            {{-- <div class="col-md-6 slide-in-right mt-5">
+                <img class="img-thumbnail zoom-in-out" src="{{ asset('frontend/theam/assets/images/about-sell/about.jpg') }}" width="520px"
+                    alt="">
+            </div> --}}
         </div>
-        <div class="col-md-6 slide-in-left">
-            <p>
-            <h3>About  <span style="color:#4baf47">Over Work</span></h3>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro, maxime quasi. Sequi non quam vero est saepe
-            soluta adipisci ea expedita, dolores possimus accusamus placeat laboriosam et provident eligendi veniam in
-            voluptates eius quidem. Consequatur libero ratione, autem ducimus, recusandae optio voluptatem corrupti
-            aliquid molestiae pariatur sequi. Eligendi sunt odio ea repellat pariatur dolorum consectetur. Et dolorem,
-            suscipit similique amet accusantium minus ipsum! Ipsam tempore illo totam sit non eos maiores et modi
-            </p>
-        </div>
-        <div class="col-md-12 slide-in-right mt-3">
-            <p class="mt-4">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, perspiciatis. Cumque illum impedit qui
-                optio, et tempora asperiores eaque, accusantium veritatis ex quaerat sequi repellendus vero ipsum saepe
-                voluptates. Nisi nostrum ullam totam quibusdam molestiae alias iure sit odio! Quia ad veniam dolor
-            </p>
-        </div>
-        {{-- <div class="col-md-6 slide-in-right mt-5">
-            <img class="img-thumbnail zoom-in-out" src="{{ asset('frontend/theam/assets/images/about-sell/about.jpg') }}" width="520px"
-                alt="">
-        </div> --}}
     </div>
-</div>
+</section>
 <x-frontend-footer />
