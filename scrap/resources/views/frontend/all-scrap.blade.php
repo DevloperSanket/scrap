2fe9c6313d0d3ab9f107393fdbe5ffcd756e3113
maheshsaveasweb <mahesh.saveasweb101@gmail.com>
2024-03-12 17:46:08 +0530

    changes by mahesh

--- scrap/resources/views/frontend/all-scrap.blade.php
+++ scrap/resources/views/frontend/all-scrap.blade.php
@@ -6,189 +6,189 @@
         </div>
         <div class="container">
             <div class="row d-flex">
-                <div class="card m-3 col-2">
-                    <img src="{{ asset('frontend/theam/assets/images/scrap-type/cotton.jpg') }}" height="120px"
-                        class="card-img-top" alt="...">
+                <div class="card  m-3 col-md-2">
+                    <img src="{{ asset('frontend/theam/assets/images/scrap-type/cotton.jpg') }}" height="120px" class="card-img-top" alt="...">
 
                     <div class="card-body">
-                        <h5 class="card-title font-weight-bold text-center">Cotton Clothes</h5>
+                        <h6 class="card-title font-weight-bold text-center">Cotton Clothes</h6>
+                        <h5 class="card-title font-weight-bold text-center">₹4/- KG</h5>
+                    </div>
+                </div>
+                <div class="card  m-3 col-md-2">
+                    <img src="{{ asset('frontend/theam/assets/images/scrap-type/mpl-plastic.jpg') }}" height="120px" class="card-img-top" alt="...">
+
+                    <div class="card-body">
+                        <h6 class="card-title font-weight-bold text-center">MLP - Plastic</h6>
+                        <h5 class="card-title font-weight-bold text-center">₹4/- KG</h5>
+                    </div>
+                </div>
+                <div class="card m-3 col-md-2">
+                    <img src="{{ asset('frontend/theam/assets/images/scrap-type/newspaper.jpg') }}" height="120px" class="card-img-top" alt="...">
+
+                    <div class="card-body">
+                        <h6 class="card-title font-weight-bold text-center">Newspaper</h6>
+                        <h5 class="card-title font-weight-bold text-center">₹13/- KG</h5>
                     </div>
                 </div>
                 <div class="card m-3 col-2">
-                    <img src="{{ asset('frontend/theam/assets/images/scrap-type/mpl-plastic.jpg') }}" height="120px"
-                        class="card-img-top" alt="...">
+                    <img src="{{ asset('frontend/theam/assets/images/scrap-type/Carton-box.jpg') }}" height="120px" class="card-img-top" alt="...">
 
                     <div class="card-body">
-                        <h5 class="card-title font-weight-bold text-center">MLP - Plastic</h5>
+                        <h6 class="card-title font-weight-bold text-center">Carton (Corrugated Box)</h6>
+                        <h5 class="card-title font-weight-bold text-center">₹10/- KG</h5>
                     </div>
                 </div>
                 <div class="card m-3 col-2">
-                    <img src="{{ asset('frontend/theam/assets/images/scrap-type/newspaper.jpg') }}" height="120px"
-                        class="card-img-top" alt="...">
+                    <img src="{{ asset('frontend/theam/assets/images/scrap-type/oil-dabba.jpg') }}" height="120px" class="card-img-top" alt="...">
 
                     <div class="card-body">
-                        <h5 class="card-title font-weight-bold text-center">Newspaper</h5>
+                        <h6 class="card-title font-weight-bold text-center">Oil Daba (Maximum Qty-5)</h6>
+                        <h5 class="card-title font-weight-bold text-center">₹24/- NOS</h5>
                     </div>
                 </div>
                 <div class="card m-3 col-2">
-                    <img src="{{ asset('frontend/theam/assets/images/scrap-type/Carton-box.jpg') }}" height="120px"
-                        class="card-img-top" alt="...">
+                    <img src="{{ asset('frontend/theam/assets/images/scrap-type/notebooks.jpg') }}" height="120px" class="card-img-top" alt="...">
 
                     <div class="card-body">
-                        <h5 class="card-title font-weight-bold text-center">Carton (Corrugated Box)</h5>
+                        <h6 class="card-title font-weight-bold text-center">Notebooks / Books</h6>
+                        <h5 class="card-title font-weight-bold text-center">₹10/- KG</h5>
                     </div>
                 </div>
                 <div class="card m-3 col-2">
-                    <img src="{{ asset('frontend/theam/assets/images/scrap-type/oil-dabba.jpg') }}" height="120px"
-                        class="card-img-top" alt="...">
+                    <img src="{{ asset('frontend/theam/assets/images/scrap-type/office-scrap.jpg') }}" height="120px" class="card-img-top" alt="...">
 
                     <div class="card-body">
-                        <h5 class="card-title font-weight-bold text-center">Oil Daba (Maximum Qty-5)</h5>
+                        <h class="card-title font-weight-bold text-center">Office Papers</h6>
+                            <h5 class="card-title font-weight-bold text-center">₹10/- KG</h5>
                     </div>
                 </div>
                 <div class="card m-3 col-2">
-                    <img src="{{ asset('frontend/theam/assets/images/scrap-type/notebooks.jpg') }}" height="120px"
-                        class="card-img-top" alt="...">
+                    <img src="{{ asset('frontend/theam/assets/images/scrap-type/magazine.jpg') }}" height="120px" class="card-img-top" alt="...">
 
                     <div class="card-body">
-                        <h5 class="card-title font-weight-bold text-center">Notebooks / Books</h5>
+                        <h6 class="card-title font-weight-bold text-center">Magazine</h6>
+                        <h5 class="card-title font-weight-bold text-center">₹8/- KG</h5>
                     </div>
                 </div>
                 <div class="card m-3 col-2">
-                    <img src="{{ asset('frontend/theam/assets/images/scrap-type/office-scrap.jpg') }}" height="120px"
-                        class="card-img-top" alt="...">
+                    <img src="{{ asset('frontend/theam/assets/images/scrap-type/heavy-iron.jpg') }}" height="120px" class="card-img-top" alt="...">
 
                     <div class="card-body">
-                        <h5 class="card-title font-weight-bold text-center">Office Papers</h5>
+                        <h6 class="card-title font-weight-bold text-center">Heavy Iron</h6>
+                        <h5 class="card-title font-weight-bold text-center">₹25/- KG</h5>
                     </div>
                 </div>
                 <div class="card m-3 col-2">
-                    <img src="{{ asset('frontend/theam/assets/images/scrap-type/magazine.jpg') }}" height="120px"
-                        class="card-img-top" alt="...">
+                    <img src="{{ asset('frontend/theam/assets/images/scrap-type/vehical-scrap.jpg') }}" height="120px" class="card-img-top" alt="...">
 
                     <div class="card-body">
-                        <h5 class="card-title font-weight-bold text-center">Magazine</h5>
-                    </div>
-                </div>
-                <div class="card m-3 col-2">
-                    <img src="{{ asset('frontend/theam/assets/images/scrap-type/heavy-iron.jpg') }}" height="120px"
-                        class="card-img-top" alt="...">
-
-                    <div class="card-body">
-                        <h5 class="card-title font-weight-bold text-center">Heavy Iron</h5>
-                    </div>
-                </div>
-                <div class="card m-3 col-2">
-                    <img src="{{ asset('frontend/theam/assets/images/scrap-type/vehical-scrap.jpg') }}" height="120px"
-                        class="card-img-top" alt="...">
-
-                    <div class="card-body">
-                        <h5 class="card-title font-weight-bold text-center">Vehicle Scrap</h5>
-                    </div>
-                </div>
-
-                <div class="card m-3 col-2">
-                    <img src="{{ asset('frontend/theam/assets/images/scrap-type/mix-plastics.jpg') }}" height="120px"
-                        class="card-img-top" alt="...">
-
-                    <div class="card-body">
-                        <h5 class="card-title font-weight-bold text-center">Mix Plastic</h5>
+                        <h6 class="card-title font-weight-bold text-center">Vehicle Scrap</h6>
+                        <h5 class="card-title font-weight-bold text-center">₹0/- KG</h5>
                     </div>
                 </div>
 
                 <div class="card m-3 col-2">
-                    <img src="{{ asset('frontend/theam/assets/images/scrap-type/steel.jpeg') }}" height="120px"
-                        class="card-img-top" alt="...">
+                    <img src="{{ asset('frontend/theam/assets/images/scrap-type/mix-plastics.jpg') }}" height="120px" class="card-img-top" alt="...">
+
                     <div class="card-body">
-                        <h5 class="card-title font-weight-bold text-center">Stainless Steel</h5>
+                        <h6 class="card-title font-weight-bold text-center">Mix Plastic</h6>
+                        <h5 class="card-title font-weight-bold text-center">₹12/- KG</h5>
                     </div>
                 </div>
 
                 <div class="card m-3 col-2">
-                    <img src="{{ asset('frontend/theam/assets/images/scrap-type/aluminiam.png') }}" height="120px"
-                        class="card-img-top" alt="...">
+                    <img src="{{ asset('frontend/theam/assets/images/scrap-type/steel.jpeg') }}" height="120px" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title font-weight-bold text-center">Aluminium</h5>
-                    </div>
-                </div>
-                <div class="card m-3 col-2">
-                    <img src="{{ asset('frontend/theam/assets/images/scrap-type/copper.jpg') }}" height="120px"
-                        class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title font-weight-bold text-center">Copper</h5>
-                    </div>
-                </div>
-                <div class="card m-3 col-2">
-                    <img src="{{ asset('frontend/theam/assets/images/scrap-type/brass.jpg') }}" height="120px"
-                        class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title font-weight-bold text-center">Brass</h5>
-                    </div>
-                </div>
-                <div class="card m-3 col-2">
-                    <img src="{{ asset('frontend/theam/assets/images/scrap-type/washing-machine.jpg') }}" height="120px"
-                        class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title font-weight-bold text-center">Washing Machine</h5>
-                    </div>
-                </div>
-                <div class="card m-3 col-2">
-                    <img src="{{ asset('frontend/theam/assets/images/scrap-type/fridge.jpg') }}" height="120px"
-                        class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title font-weight-bold text-center">Refrigerator</h5>
-                    </div>
-                </div>
-                <div class="card m-3 col-2">
-                    <img src="{{ asset('frontend/theam/assets/images/scrap-type/computer-sate.jpg') }}" height="120px"
-                        class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title font-weight-bold text-center">Computer Full Sate</h5>
-                    </div>
-                </div>
-                <div class="card m-3 col-2">
-                    <img src="{{ asset('frontend/theam/assets/images/scrap-type/microwave.jpg') }}" height="120px"
-                        class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title font-weight-bold text-center">Microwave</h5>
+                        <h6 class="card-title font-weight-bold text-center">Stainless Steel</h6>
+                        <h5 class="card-title font-weight-bold text-center">₹40/- KG</h5>
                     </div>
                 </div>
 
                 <div class="card m-3 col-2">
-                    <img src="{{ asset('frontend/theam/assets/images/scrap-type/printer-scanner.jpg') }}" height="120px"
-                        class="card-img-top" alt="...">
+                    <img src="{{ asset('frontend/theam/assets/images/scrap-type/aluminiam.png') }}" height="120px" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title font-weight-bold text-center">Printer/Scanner</h5>
+                        <h6 class="card-title font-weight-bold text-center">Aluminium</h6>
+                        <h5 class="card-title font-weight-bold text-center">₹115/- KG</h5>
+                    </div>
+                </div>
+                <div class="card m-3 col-2">
+                    <img src="{{ asset('frontend/theam/assets/images/scrap-type/copper.jpg') }}" height="120px" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h6 class="card-title font-weight-bold text-center">Copper</h6>
+                        <h5 class="card-title font-weight-bold text-center">₹380/- KG</h5>
+                    </div>
+                </div>
+                <div class="card m-3 col-2">
+                    <img src="{{ asset('frontend/theam/assets/images/scrap-type/brass.jpg') }}" height="120px" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h6 class="card-title font-weight-bold text-center">Brass</h6>
+                        <h5 class="card-title font-weight-bold text-center">₹330/- KG</h5>
+                    </div>
+                </div>
+                <div class="card m-3 col-2">
+                    <img src="{{ asset('frontend/theam/assets/images/scrap-type/washing-machine.jpg') }}" height="120px" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h6 class="card-title font-weight-bold text-center">Washing Machine</h6>
+                        <h5 class="card-title font-weight-bold text-center">₹24/- KG</h5>
+                    </div>
+                </div>
+                <div class="card m-3 col-2">
+                    <img src="{{ asset('frontend/theam/assets/images/scrap-type/fridge.jpg') }}" height="120px" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h6 class="card-title font-weight-bold text-center">Refrigerator</h6>
+                        <h5 class="card-title font-weight-bold text-center">₹24/- KG</h5>
+                    </div>
+                </div>
+                <div class="card m-3 col-2">
+                    <img src="{{ asset('frontend/theam/assets/images/scrap-type/computer-sate.jpg') }}" height="120px" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h6 class="card-title font-weight-bold text-center">Computer Full Sate</h6>
+                        <h5 class="card-title font-weight-bold text-center">₹350/- NOS</h5>
+                    </div>
+                </div>
+                <div class="card m-3 col-2">
+                    <img src="{{ asset('frontend/theam/assets/images/scrap-type/microwave.jpg') }}" height="120px" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h6 class="card-title font-weight-bold text-center">Microwave</h6>
+                        <h5 class="card-title font-weight-bold text-center">₹24/- KG</h5>
                     </div>
                 </div>
 
                 <div class="card m-3 col-2">
-                    <img src="{{ asset('frontend/theam/assets/images/scrap-type/pipes.jpg') }}" height="120px"
-                        class="card-img-top" alt="...">
+                    <img src="{{ asset('frontend/theam/assets/images/scrap-type/printer-scanner.jpg') }}" height="120px" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title font-weight-bold text-center">Pipes</h5>
+                        <h6 class="card-title font-weight-bold text-center">Printer/Scanner</h6>
+                        <h5 class="card-title font-weight-bold text-center">₹24/- KG</h5>
                     </div>
                 </div>
 
                 <div class="card m-3 col-2">
-                    <img src="{{ asset('frontend/theam/assets/images/scrap-type/ac.jpg') }}" height="120px"
-                        class="card-img-top" alt="...">
+                    <img src="{{ asset('frontend/theam/assets/images/scrap-type/pipes.jpg') }}" height="120px" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title font-weight-bold text-center">Air Conditioner (AC)</h5>
+                        <h6 class="card-title font-weight-bold text-center">Pipes</h6>
+                        <h5 class="card-title font-weight-bold text-center">₹13/- KG</h5>
+                    </div>
+                </div>
+
+                <div class="card m-3 col-2">
+                    <img src="{{ asset('frontend/theam/assets/images/scrap-type/ac.jpg') }}" height="120px" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h6 class="card-title font-weight-bold text-center">Air Conditioner (AC)</h6>
+                        <h5 class="card-title font-weight-bold text-center">₹0/- NOS</h5>
                     </div>
                 </div>
                 <div class="card m-3 col-2">
-                    <img src="{{ asset('frontend/theam/assets/images/scrap-type/tv.png') }}" height="120px"
-                        class="card-img-top" alt="...">
+                    <img src="{{ asset('frontend/theam/assets/images/scrap-type/tv.png') }}" height="120px" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title font-weight-bold text-center">TV</h5>
+                        <h6 class="card-title font-weight-bold text-center">TV</h6>
+                        <h5 class="card-title font-weight-bold text-center">₹130/- NOS</h5>
                     </div>
                 </div>
                 <div class="card m-3 col-2">
-                    <img src="{{ asset('frontend/theam/assets/images/scrap-type/tyres.jpg') }}" height="120px"
-                        class="card-img-top" alt="...">
+                    <img src="{{ asset('frontend/theam/assets/images/scrap-type/tyres.jpg') }}" height="120px" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title font-weight-bold text-center">Tyres</h5>
+                        <h6 class="card-title font-weight-bold text-center">Tyres</h6>
+                        <h5 class="card-title font-weight-bold text-center">₹4/- KG</h5>
                     </div>
                 </div>
             </div>
