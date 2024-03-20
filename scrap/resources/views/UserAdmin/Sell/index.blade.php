935f130b8d6e89fa1cec1cbc95b1745bb86874ae
nirmala-coder2 <nirmala.saveasweb@gmail.com>
2024-03-20 14:18:46 +0530

    push

--- scrap/resources/views/UserAdmin/Sell/index.blade.php
+++ scrap/resources/views/UserAdmin/Sell/index.blade.php
@@ -55,6 +55,23 @@
             </div>
         </div>
     </section>
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Scrap Images</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    ...
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </main><!-- End #main -->
 <x-admin-footer />
 <script>
@@ -94,14 +111,15 @@
                 {
                     data: 'image',
                     name: 'image',
-                    render: function(data, type, row, meta) {
-                        // var imagePath = '{{ asset('') }}' + data;
-                        return '<img src="' + data +
-                            '" alt="Card Image" style="max-width: 100px; max-height: 50px;">';
-                    }
                 }
             ]
         });
 
     });
+
+    function showData(imageUrl) {
+        var assetUrl = "{{ asset('') }}" + imageUrl; // Concatenating imageUrl with asset root
+        $('#exampleModal .modal-body').html('<img class="col-3" src="' + assetUrl + '" class="img-fluid">');
+        $('#exampleModal').modal('show');
+    }
 </script>
